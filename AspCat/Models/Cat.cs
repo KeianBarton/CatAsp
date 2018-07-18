a7f4e183626e1170dab0c54ca517a4cdbb2234aa
KeianBarton <Keian.Barton@civica.co.uk>
2018-07-18 10:14:01 +0100

    Initial solution

---
+++ AspCat/Models/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AspCat.Models
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Breed Breed { get; set; }
+
+        public double Weight { get; set; }
+
+        public ApplicationUser Owner { get; set; }
+    }
+}
